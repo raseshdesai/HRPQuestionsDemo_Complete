f0a85692ca552dd136447ee1cee1010d6097173d
Rasesh Desai <rasesh.desai@gmail.com>
2013-06-24 15:39:25 -0500

    Initial Commit

---
+++ HRPMobilityQuestionsApp_Demo_1/HRPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HRPAppDelegate.h
+//  HRPMobilityQuestionsApp_Demo_1
+//
+//  Created by Rasesh Desai on 6/24/13.
+//  Copyright (c) 2013 Rasesh Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HRPMobilityQuestionsApp_Demo_1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HRPMobilityQuestionsApp_Demo_1
+//
+//  Created by Rasesh Desai on 6/24/13.
+//  Copyright (c) 2013 Rasesh Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HRPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HRPAppDelegate class]));
+    }
+}
